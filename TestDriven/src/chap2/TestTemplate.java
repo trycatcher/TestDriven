9586df96f544a4ff3e55ab33b5e6ab51d20afb39
Abhik Khanra <abhik.rk@gmail.com>
2015-03-16 17:17:09 +0530

    Refactored tests

--- TestDriven/src/chap2/TestTemplate.java
+++ TestDriven/src/chap2/TestTemplate.java
@@ -2,45 +2,32 @@ package chap2;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestTemplate {
+	private Template template;
 	
-	@Test
-	public void oneVariable() throws Exception {
-		Template template = new Template("Hello, ${name}");
-		template.set("name", "Reader");
-		assertEquals("Hello, Reader", template.evaluate());
-	}
-	
-	@Test
-	public void differentValue() throws Exception {
-		Template template = new Template("Hello, ${name}");
-		template.set("name", "someone else");
-		assertEquals("Hello, someone else", template.evaluate());
-	}
-	
-	@Test
-	public void differentTemplate() throws Exception {
-		Template template = new Template("Hi, ${name}");
-		template.set("name", "someone else");
-		assertEquals("Hi, someone else", template.evaluate());
+	@Before
+	public void setUp() throws Exception {
+		template = new Template("${one}, ${two}, ${three}");
+		template.set("one", "1");
+		template.set("two", "2");
+		template.set("three", "3");
 	}
 	
 	@Test
 	public void multipleVariables() throws Exception {
-		Template template = new Template("${one}, ${two}, ${three}");
-		template.set("one", "1");
-		template.set("two", "2");
-		template.set("three", "3");
-		assertEquals("1, 2, 3", template.evaluate());
+		assertTemplateEvaluatesTo("1, 2, 3");
 	}
-	
+
 	@Test
 	public void unknownVariablesAreIgnored() throws Exception {
-		Template template = new Template("Hello, ${name}");
-		template.set("name", "Reader");
 		template.set("doesnotexist", "hi");
-		assertEquals("Hello, Reader", template.evaluate());
+		assertTemplateEvaluatesTo("1, 2, 3");
+	}
+	
+	private void assertTemplateEvaluatesTo(String templateText) {
+		assertEquals(templateText, template.evaluate());		
 	}
 }
